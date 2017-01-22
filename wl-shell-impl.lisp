c6b57985e120c2fa7c047c67c86841a9402798ae
Malcolm Still <malcolm.still@gmail.com>
2017-01-22 18:12:05 +0000

    Removed commented code from desktop-mode.lisp. Fixed wl-subcompositor-impl.lisp so that the parent of subsurface is set to (role parent) rather than just parent. Removed commented code from wl-shell-impl.lisp.

--- wl-shell-impl.lisp
+++ wl-shell-impl.lisp
@@ -18,20 +18,3 @@
 
 (def-wl-bind shell-bind (client (data :pointer) (version :uint32) (id :uint32))
   (make-wl-shell client 1 id))
-
-#|
-(defcallback shell-bind :void ((client-ptr :pointer) (data :pointer) (version :uint32) (id :uint32))
-  (bind-wl-shell client-ptr 1 id))
-|#
-
-#|
-(defcallback shell-bind :void
-  ((client-ptr :pointer) (data :pointer) (version :uint32) (id :uint32))
-  (format t "~%shell-bind called: ~A~%" client-ptr)
-  (waylisp:get-client client-ptr)
-  (wl-resource-set-implementation
-   (wl-resource-create client-ptr wl-shell-interface 1 id)
-   wl-shell-implementation
-   client-ptr
-   (null-pointer)));;(callback client-destroy)))
-|#
